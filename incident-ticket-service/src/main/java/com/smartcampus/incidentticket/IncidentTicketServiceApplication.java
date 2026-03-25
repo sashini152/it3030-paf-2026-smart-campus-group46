6d03234ed4058ead9dcc03fe2f992f63cc69465c
Hafzan Ahamed <hafzanahame99@gmail.com>
2026-03-26 00:06:42 +0530

    feat: add Ticket entity with enums

--- incident-ticket-service/src/main/java/com/smartcampus/incidentticket/IncidentTicketServiceApplication.java
+++ incident-ticket-service/src/main/java/com/smartcampus/incidentticket/IncidentTicketServiceApplication.java
@@ -2,8 +2,10 @@ package com.smartcampus.incidentticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class IncidentTicketServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IncidentTicketServiceApplication.class, args);
