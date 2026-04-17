32431a6c4a1c143dc5874d21c07333d9d9d99955
dewmini03 <chamodyadewmini08@gmail.com>
2026-04-17 20:04:45 +0530

    Add admin gmail

--- frontend/src/constants/auth.js
+++ frontend/src/constants/auth.js
@@ -1,6 +1,6 @@
 export const ADMIN_EMAILS = [
   'it23220492@my.sliit.lk',
-  'it23219120@my.sliit.lk',
+  'chamodyadewmini08@gmail.com',
   'it23207936@my.sliit.lk',
   'it23239470@my.sliit.lk'
 ].map(email => email.toLowerCase())
