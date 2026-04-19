b2dd9f0d5903547e93db446562a878e77d9670f2
dewmini03 <chamodyadewmini08@gmail.com>
2026-04-19 10:22:21 +0530

    change css

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -61,8 +61,7 @@ export default function RegisterPage() {
 
           <div className="nav-links">
             <Link to="/">Home</Link>
-<Link to="/login">Sign in</Link>
-<Link to="/register">Sign up</Link>
+            <Link to="/login">Sign in</Link>
           </div>
         </div>
       </div>
